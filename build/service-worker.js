449ebf52eec98e91bc25848f3aacaeb090cd4bf0
supersorbet <artificialperiod@gmail.com>
2023-04-08 08:40:24 -0400

    5

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,39 +1,12 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/precache-manifest.3b36c95bd7f0be5e8d191dc7900b893e.js"
-);
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+self.addEventListener('install', (event) => {
+    console.log('Service worker installed');
+  });
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service worker activated');
+  });
+  
