d37e15ba8b40063dfd4ae5fb5711a35efa65c175
supersorbet <artificialperiod@gmail.com>
2023-04-08 08:46:48 -0400

    mega json

---
+++ build/precache-manifest.a0f807d3d927cb3ed23d006b0abe8742.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f1026537ba9c30a3c38780ee1c227d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3485251b0dca121ec9c9",
+    "url": "/static/css/main.e14d4269.chunk.css"
+  },
+  {
+    "revision": "ada731362df44aea1b61",
+    "url": "/static/js/2.c5cd1949.chunk.js"
+  },
+  {
+    "revision": "31ab0097190a9d99b331909bb0d9d9b1",
+    "url": "/static/js/2.c5cd1949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3485251b0dca121ec9c9",
+    "url": "/static/js/main.63741d0b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.63741d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a697aa4a5ab5274ffda5",
+    "url": "/static/js/runtime-main.e85ef440.js"
+  },
+  {
+    "revision": "61970374dd286c4c7fa3c0c37a658aff",
+    "url": "/static/media/bg-element.61970374.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -1,12 +1,39 @@
-self.addEventListener('install', (event) => {
-    console.log('Service worker installed');
-  });
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.a0f807d3d927cb3ed23d006b0abe8742.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
-  });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Service worker activated');
-  });
-  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
